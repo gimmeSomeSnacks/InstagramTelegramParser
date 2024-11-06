34ad10423bd8059ccfc2b6205a2bc5dbecc4f9ef
nikita <n-tuganov@mail.ru>
2024-11-06 20:36:11 +0300

    bug fixes

--- src/main/java/ru/tuganov/Message.java
+++ src/main/java/ru/tuganov/Message.java
@@ -4,7 +4,7 @@ public class Message {
     public static String noUrls = "Неправильный формат ввода.";
     public static String returnVideos = "Весь шедевро-контент скачан:";
     public static String help = "Пример правильных ссылок на видосы (рилсы): \n1)https://www.instagram.com/reel/DA3eMJNRv2k/?utm_source=ig_web_copy_link\n" +
-            "https://www.instagram.com/reel/DA3eMJNRv2k/?utm_source=ig_web_copy_link" +
+            "https://www.instagram.com/reel/DA3eMJNRv2k/?utm_source=ig_web_copy_link\n" +
             "2)https://www.instagram.com/reel/DA3eMJNRv2k/?utm_source=ig_web_copy_link https://www.instagram.com/reel/DA3eMJNRv2k/?utm_source=ig_web_copy_link";
 
 }
